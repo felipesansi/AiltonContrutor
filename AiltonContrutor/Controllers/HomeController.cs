5a66eef8f34d094084a7d072b2394bd3b9457223
Felipe Freitas dos Santos Silva <felipeprofissional22@gmail.com>
2025-01-17 16:09:35 -0300

    View contato concluida

--- AiltonContrutor/Controllers/HomeController.cs
+++ AiltonContrutor/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace AiltonContrutor.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public ActionResult Agradecimento()
+        {
+            return View();
+        }
+
     }
 }
