f05374ad6530fbc0a92c2a22fde2686452e7f91e
Felipe Freitas dos Santos Silva <felipeprofissional22@gmail.com>
2025-01-29 14:54:27 -0300

    Token refresh feito

---
+++ AiltonContrutor/Models/TokenResposta.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace AiltonContrutor.Models
+{
+    public class TokenResposta
+    {
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+        [JsonProperty("token_type")]
+        public string TokenType { get; set; }
+        [JsonProperty("expires_in")]
+        public int ExpiresIn { get; set; }
+    }
+}
