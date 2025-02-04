7beaa69115970e4bf0a5dfb2776d8d009f759541
Felipe Freitas dos Santos Silva <felipeprofissional22@gmail.com>
2025-02-04 16:21:27 -0300

    View Detalhes feita

--- AiltonContrutor/Controllers/ImovelController.cs
+++ AiltonContrutor/Controllers/ImovelController.cs
@@ -1,17 +1,22 @@
 ﻿using AiltonConstrutor.Repositorio.Interfaces;
+using AiltonContrutor.Context;
 using AiltonContrutor.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AiltonContrutor.Controllers
 {
     public class ImovelController : Controller
     {
       private readonly IImovel _imovelRepositorio;
+      private readonly AppDbContext _context;
 
-        public ImovelController(IImovel imovelRepositorio)
+        public ImovelController(IImovel imovelRepositorio, AppDbContext context)
         {
             _imovelRepositorio = imovelRepositorio;
+            _context = context;
         }
+
         public IActionResult List()
         {
             var vmImovel = new ImovelViewModel(); // Instancia a ViewModel
@@ -21,5 +26,17 @@ namespace AiltonContrutor.Controllers
             return View(vmImovel); // Retorna a ViewModel para a view
 
         }
+        public async Task<IActionResult> Detalhes(int id)
+        {
+            var imovel = await _context.Imoveis
+                .Include(i => i.Fotos) // Carrega as fotos associadas ao imóvel
+                .FirstOrDefaultAsync(i => i.IdImovel == id);
+
+            if (imovel == null)
+                return NotFound();
+
+            return View(imovel);
+        }
+
     }
 }
