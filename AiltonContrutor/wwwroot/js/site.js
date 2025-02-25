4282500b6bba0f6cef1232e2d70d26dd5a435172
Felipe Freitas dos Santos Silva <felipeprofissional22@gmail.com>
2025-02-25 15:33:35 -0300

    Final

--- AiltonContrutor/wwwroot/js/site.js
+++ AiltonContrutor/wwwroot/js/site.js
@@ -1,1 +1,6 @@
-﻿AOS.init();
+﻿// Inicializa as animações AOS
+AOS.init({
+    duration: 1000, // Duração das animações
+    easing: "ease-in-out", // Tipo de suavização
+    once: true, // Anima apenas uma vez
+});
